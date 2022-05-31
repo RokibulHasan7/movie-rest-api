66800c8cebb28022a2adc5564fa8f8683a0c4da7
RokibulHasan7 <mdrokibulhasan18@gmail.com>
2022-05-31 21:08:29 +0600

    fixed

--- movieapi/urls.py
+++ movieapi/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from movieapi.settings import MEDIA_URL
 from movies.views import MovieViewSet, ActionViewSet, SuperheroViewSet
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies', MovieViewSet)
@@ -26,4 +30,5 @@ router.register('superhero', SuperheroViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
